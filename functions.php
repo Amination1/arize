056e86a2221480ff908ebcf03e66e089ab31234e
Amination1 <seyyed.amin.8809@gmail.com>
2025-02-13 17:13:25 +0330

    new router

--- functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-function view_c($filename) {
-    require "controllers/$filename.php";
-}
-
-function view($filename) {
-    require "views/$filename.view.php";
-}
--- routes.php
+++ routes.php
@@ -2,17 +2,3 @@
 
 $router = new Core\Router();
 
-// تعریف مسیرها
-$router->route('/', 'GET', function() {
-    view_c("home");
-});
-
-$router->route('/about', 'GET', function() {
-echo 'درباره ما';
-});
-
-$router->route('/create', 'POST', function() {
-    view_c('create');
-});
-
-$router->dispatch();
